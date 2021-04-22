686f9d4c723a23a0a18020f11a459cb89b1ccaae
Finn <finn.s.james@gmail.com>
2021-04-22 11:30:29 -0400

    ✨ initial commit ✨

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <TitleBar />
+    <HelloWorld msg="" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
+import TitleBar from "./components/TitleBar.vue"
+
+@Component({
+  components: {
+    HelloWorld,
+    TitleBar,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/TitleBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <div class="navbar-item">
+        <span class="title">paint541</span>
+      </div>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-end">
+        <a class="navbar-item is-spaced">
+          <span class="icon"><img src="@/assets/svg/gh.svg" /></span>
+          <span>GitHub</span>
+        </a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class TitleBar extends Vue {}
+</script>
+
+<style lang="scss">
+.icon {
+  margin-right: 0.6rem;
+}
+</style>
